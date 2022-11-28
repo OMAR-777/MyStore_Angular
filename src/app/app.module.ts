a8c75eaae4f2f6afbcb8ba6293c7e65aeff66538
OMAR-777 <omar.omar5745@gmail.com>
2022-11-28 21:53:03 +0300

    created product list and cart component + refactor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './layout/header/header.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { ProductListComponent } from './components/product-list/product-list.component';
+import { CartComponent } from './components/cart/cart.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    ProductListComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
