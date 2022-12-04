647dc593935011337d938609f0b9491265b5c914
OMAR-777 <omar.omar5745@gmail.com>
2022-12-04 19:47:39 +0300

    validate credit card as a number

--- src/app/services/cart.service.ts
+++ src/app/services/cart.service.ts
@@ -20,7 +20,7 @@ export class CartService {
 
   getCartAmount(): number{
     let sum = 0;
-    this.cartItems.forEach(ci => sum+=ci.quantity)
+    this.cartItems.forEach(ci => sum+=+ci.quantity)
     return sum;
   }
 
